10db617a98be4ed390198f68e6726152c6fe52e4
yakhyo04 <fenixrelaxation@gmail.com>
2021-10-22 10:32:01 +0500

    some changes

---
+++ script.js
@@ -0,0 +1,8 @@
+const elMenu = document.querySelector('#menu');
+const elHeaderBtn = document.querySelector('.extra__btn');
+const elMenuExtra = document.querySelector('.menu__extra')
+
+elMenu.addEventListener('click', (e)=>{
+    e.preventDefault();
+ 
+})
